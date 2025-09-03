ec5c4503bd6f89e27fe802f93381a83f5b7dbfc1
Josh Caponigro <joshcaponigro@protonmail.com>
2025-09-03 00:21:24 -0500

    Add client request handling base

--- src/connection/request.rs
+++ src/connection/request.rs
@@ -6,33 +6,34 @@ use tokio::{
 };
 use tracing::{debug, error};
 
+#[derive(Debug)]
+pub struct SocksRequest {
+    pub version: u8,
+    pub command: u8,
+    pub reserved: u8,
+    pub address_type: u8,
+    pub dest_addr: std::net::IpAddr,
+    pub dest_port: u16,
+}
+
 pub async fn handle_request(
     reader: &mut BufReader<OwnedReadHalf>,
     writer: &mut BufWriter<OwnedWriteHalf>,
     client_addr: SocketAddr,
 ) -> io::Result<()> {
-///    The SOCKS request is formed as follows:
-///        +----+-----+-------+------+----------+----------+
-///        |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
-///        +----+-----+-------+------+----------+----------+
-///        | 1  |  1  | X'00' |  1   | Variable |    2     |
-///        +----+-----+-------+------+----------+----------+
-///
-///     Where:
-///          o  VER    protocol version: X'05'
-///          o  CMD
-///             o  CONNECT X'01'
-///             o  BIND X'02'
-///             o  UDP ASSOCIATE X'03'
-///          o  RSV    RESERVED
-///          o  ATYP   address type of following address
-///             o  IP V4 address: X'01'
-///             o  DOMAINNAME: X'03'
-///             o  IP V6 address: X'04'
-///          o  DST.ADDR       desired destination address
-///          o  DST.PORT desired destination port in network octet
-///             order
     debug!("Handling request from {}", client_addr);
+
+    let client_request = parse_request(reader, client_addr).await?;
+    handle_client_request(client_request, writer, client_addr).await?;
+
+    Ok(())
+}
+
+async fn parse_request(
+    reader: &mut BufReader<OwnedReadHalf>,
+    client_addr: SocketAddr,
+) -> io::Result<SocksRequest> {
+    debug!("Parsing request from {}", client_addr);
 
     let version = reader.read_u8().await?;
     let command = reader.read_u8().await?;
@@ -72,5 +73,45 @@ pub async fn handle_request(
         }
     };
     let dest_port = reader.read_u16().await?;
+    debug!(
+        "Client {} wants to connect to {} : {}",
+        client_addr, dest_addr, dest_port
+    );
+    Ok(SocksRequest {
+        version,
+        command,
+        reserved,
+        address_type,
+        dest_addr,
+        dest_port,
+    })
+}
+
+async fn handle_client_request(
+    client_request: SocksRequest,
+    writer: &mut BufWriter<OwnedWriteHalf>,
+    client_addr: SocketAddr,
+) -> io::Result<()> {
+    debug!("Handling client request from {}: {:?}", client_addr, client_request);
+
+    match client_request.command {
+        1 => {
+            debug!("Handling CONNECT request");
+        }
+        2 => {
+            debug!("Handling BIND request");
+        }
+        3 => {
+            debug!("Handling UDP ASSOCIATE request");
+        }
+        _ => {
+            error!("Unsupported command: {}", client_request.command);
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Unsupported command",
+            ));
+        }
+    }
+
     Ok(())
 }
