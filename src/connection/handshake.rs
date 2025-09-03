c9a325c577ac2e769fe76a6551fa0a3572ddcd32
Josh Caponigro <joshcaponigro@protonmail.com>
2025-09-02 23:54:32 -0500

    Add initial greeting request/response base

--- src/connection/handshake.rs
+++ src/connection/handshake.rs
@@ -1,10 +1,16 @@
 use std::{io, net::SocketAddr};
 
 use tokio::{
-    io::{AsyncReadExt, BufReader, BufWriter},
+    io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
     net::tcp::{OwnedReadHalf, OwnedWriteHalf},
 };
 use tracing::debug;
+
+struct HandshakeRequest {
+    version: u8,
+    nmethods: u8,
+    methods: Vec<u8>,
+}
 
 pub async fn perform_handshake(
     mut reader: BufReader<OwnedReadHalf>,
@@ -13,7 +19,20 @@ pub async fn perform_handshake(
 ) -> io::Result<()> {
     debug!("Performing handshake for client {}", client_addr);
 
+    let handshake_request = parse_client_greeting(reader, client_addr).await?;
+    let handshake_response = handle_client_greeting(handshake_request, writer, client_addr).await?;
+
+    Ok(())
+}
+
+async fn parse_client_greeting(
+    mut reader: BufReader<OwnedReadHalf>,
+    client_addr: SocketAddr,
+) -> io::Result<HandshakeRequest> {
+    debug!("Parsing client greeting for {}", client_addr);
+
     let version = reader.read_u8().await?;
+    // TODO: Validate socks 5
     let nmethods = reader.read_u8().await?;
     debug!(
         "Client {} is using SOCKS version {} with {} methods",
@@ -25,6 +44,34 @@ pub async fn perform_handshake(
     debug!(
         "Client {} supports the following authentication methods: {:?}",
         client_addr, methods
+    );
+
+    Ok(HandshakeRequest {
+        version,
+        nmethods,
+        methods,
+    })
+}
+
+async fn handle_client_greeting(
+    handshake_request: HandshakeRequest,
+    mut writer: BufWriter<OwnedWriteHalf>,
+    client_addr: SocketAddr,
+) -> io::Result<()> {
+    /// TODO: Support all authentication methods
+    ///           o  X'01' GSSAPI
+    ///           o  X'02' USERNAME/PASSWORD
+    ///           o  X'03' to X'7F' IANA ASSIGNED
+    ///           o  X'80' to X'FE' RESERVED FOR PRIVATE METHODS
+    ///           o  X'FF' NO ACCEPTABLE METHODS
+    debug!("Handling client greeting for {}", client_addr);
+
+    let response = [0x05, 0x00];
+    writer.write_all(&response).await?;
+    writer.flush().await?;
+    debug!(
+        "Sent handshake response to client {}: {:?}",
+        client_addr, response
     );
 
     Ok(())
