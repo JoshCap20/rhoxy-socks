057e44a0062f94061a55a1ddc249c88eedc36f61
Josh Caponigro <joshcaponigro@protonmail.com>
2025-09-02 23:25:44 -0500

    Add handshake ref

--- src/lib.rs
+++ src/lib.rs
@@ -29,8 +29,46 @@ async fn handle_handshake(
 ) -> io::Result<()> {
     // TODO: implement handshake handling
 
-    // 1. parse client greeting
-    /// 	VER	NAUTH	AUTH
-    /// Byte count	1	1	variable
+    /// Where a given octet must take on a specific value, the
+   /// syntax X'hh' is used to denote the value of the single octet in that
+   /// field. When the word 'Variable' is used, it indicates that the
+   /// corresponding field has a variable length defined either by an
+   /// associated (one or two octet) length field, or by a data type field.
+   ///
+   /// The client connects to the server, and sends a version
+   /// identifier/method selection message:
+
+    ///               +----+----------+----------+
+    ///               |VER | NMETHODS | METHODS  |
+    ///               +----+----------+----------+
+    ///               | 1  |    1     | 1 to 255 |
+    ///               +----+----------+----------+
+
+   /// The VER field is set to X'05' for this version of the protocol.  The
+   /// NMETHODS field contains the number of method identifier octets that
+   /// appear in the METHODS field.
+
+   /// The server selects from one of the methods given in METHODS, and
+   /// sends a METHOD selection message:
+
+    ///                     +----+--------+
+    ///                     |VER | METHOD |
+    ///                     +----+--------+
+    ///                     | 1  |   1    |
+    ///                     +----+--------+
+
+   /// If the selected METHOD is X'FF', none of the methods listed by the
+   /// client are acceptable, and the client MUST close the connection.
+
+   /// The values currently defined for METHOD are:
+
+   ///       o  X'00' NO AUTHENTICATION REQUIRED
+   ///       o  X'01' GSSAPI
+   ///       o  X'02' USERNAME/PASSWORD
+   ///       o  X'03' to X'7F' IANA ASSIGNED
+   ///       o  X'80' to X'FE' RESERVED FOR PRIVATE METHODS
+   ///       o  X'FF' NO ACCEPTABLE METHODS
+
+   /// The client and server then enter a method-specific sub-negotiation.
     Ok(())
 }
