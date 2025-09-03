db77bfe3a6bd1078dc0ff6e84ea2263d68dae9a3
Josh Caponigro <joshcaponigro@protonmail.com>
2025-09-02 22:11:03 -0500

    Setup server and connection handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use tokio::net::{TcpListener, TcpStream};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,4 +23,24 @@ async fn main() {
     } else {
         // TODO: init tracing subscriber with info level
     }
+
+    start_server(args).await;
+}
+
+async fn start_server(args: Args) {
+    let listener = TcpListener::bind(format!("{}:{}", args.host, args.port))
+        .await
+        .expect("Failed to bind to address");
+
+    loop {
+        let (socket, _) = listener
+            .accept()
+            .await
+            .expect("Failed to accept connection");
+        tokio::spawn(handle_connection(socket));
+    }
+}
+
+async fn handle_connection(socket: TcpStream) {
+    // TODO: implement connection handling
 }
