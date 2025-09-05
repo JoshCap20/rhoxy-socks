a01bd3a4fed6b9a166017eb0489189b8d2bc3c4d
Josh Caponigro <joshcaponigro@protonmail.com>
2025-09-05 02:30:55 -0500

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -28,18 +28,44 @@ pub async fn handle_connection(
     let (reader, writer) = stream.into_split();
     let mut reader = BufReader::with_capacity(config.buffer_size, reader);
     let mut writer = BufWriter::with_capacity(config.buffer_size, writer);
-    
-    match timeout(config.handshake_timeout, connection::perform_handshake(&mut reader, &mut writer, client_addr, &config.supported_auth_methods)).await {
+
+    match timeout(
+        config.handshake_timeout,
+        connection::perform_handshake(
+            &mut reader,
+            &mut writer,
+            client_addr,
+            &config.supported_auth_methods,
+        ),
+    )
+    .await
+    {
         Ok(result) => result?,
         Err(_) => {
-            debug!("Handshake timeout for {} after {:?}", client_addr, config.handshake_timeout);
+            debug!(
+                "Handshake timeout for {} after {:?}",
+                client_addr, config.handshake_timeout
+            );
             return Err(io::Error::new(io::ErrorKind::TimedOut, "Handshake timeout"));
         }
     }
-    match timeout(config.connection_timeout, connection::request::SocksRequest::handle_request(&mut reader, &mut writer, client_addr, config.tcp_nodelay)).await {
+    match timeout(
+        config.connection_timeout,
+        connection::request::SocksRequest::handle_request(
+            &mut reader,
+            &mut writer,
+            client_addr,
+            config.tcp_nodelay,
+        ),
+    )
+    .await
+    {
         Ok(result) => result?,
         Err(_) => {
-            debug!("Connection {} timed out after {:?}", client_addr, config.connection_timeout);
+            debug!(
+                "Connection {} timed out after {:?}",
+                client_addr, config.connection_timeout
+            );
             return Err(io::Error::new(
                 io::ErrorKind::TimedOut,
                 format!("Connection timed out after {:?}", config.connection_timeout),
